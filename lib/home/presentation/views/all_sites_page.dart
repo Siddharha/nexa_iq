7cf7085ff7875b82f461e97ba61fd725ffe946b5
Siddhartha Maji <siddhartha.maji@videonetics.com>
2025-09-11 18:16:07 +0530

    d2

--- lib/home/presentation/views/all_sites_page.dart
+++ lib/home/presentation/views/all_sites_page.dart
@@ -4,21 +4,25 @@ class AllSitesPage extends StatelessWidget {
   const AllSitesPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-       padding: const EdgeInsets.symmetric(vertical: 8),
-       itemCount: 20,
-       itemBuilder: (context,index){
-        return ListTile(
-          title: Text("Test", style: TextStyle(color: Colors.white),),
-          );
-    },
-    separatorBuilder: (context, index) {
-    return const Divider(
-      color: Colors.white54, // divider color
-      thickness: 0.5,          // divider thickness
-      indent: 16,            // left padding
-      endIndent: 16,         // right padding
+    return Container(
+      color: Color(0xFF03171f),
+      child: ListView.separated(
+         padding: const EdgeInsets.symmetric(vertical: 8),
+         itemCount: 20,
+         itemBuilder: (context,index){
+          return ListTile(
+            title: Text("Test", style: TextStyle(color: Colors.white),),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            );
+      },
+      separatorBuilder: (context, index) {
+      return const Divider(
+        color: Colors.white54, // divider color
+        thickness: 0.5,          // divider thickness
+        indent: 16,            // left padding
+        endIndent: 16,         // right padding
+      );
+        },),
     );
-      },);
   }
 }
